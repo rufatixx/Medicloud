e311de4462978413285c023751a798c92f2ac3a7
rufat <Asadzade99@gmail.com>
2024-09-08 23:48:14 +0400

    added active or closed badgge to cards in the Prescriptions ppage

--- FlexitHisMVC/BLL/Models/PatientDocDTO.cs
+++ FlexitHisMVC/BLL/Models/PatientDocDTO.cs
@@ -10,6 +10,7 @@ namespace Medicloud.Models
         public long serviceID { get; set; }
         public long patientCardID { get; set; }
         public int totalCardNumbers { get; set; }
+        public int finished { get; set; }
         public string note { get; set; }
        
     }
